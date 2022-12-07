fb4647e86025fb0f40067c047f17911e45f62830
Trigo YODILA <trigoyodila1996@gmail.com>
2022-12-07 15:27:18 +0100

    Feat:resolve second part

--- day3/Functions.js
+++ day3/Functions.js
@@ -1,5 +1,6 @@
 const default_value = require("./Letters");
 
+//Function part I
 const getRucksackNumberOfCompartiments = (bag) => {
   const firstCompartiment = bag.slice(0, bag.length / 2);
   const secondCompartiment = bag.slice(bag.length / 2);
@@ -17,4 +18,30 @@ const getNumberOfKeyArticle = (key) => {
   return default_value.indexOf(key) + 1;
 };
 
-module.exports = getRucksackNumberOfCompartiments;
+//Functions Part 2
+const getKeyOfGroup = (group) => {
+  const firstSelf = group[0];
+  const secondSelf = group[1];
+  const thirdSelf = group[2];
+
+  for (let i = 0; i < default_value.length; i++) {
+    if (
+      firstSelf.includes(default_value[i]) &&
+      secondSelf.includes(default_value[i]) &&
+      thirdSelf.includes(default_value[i])
+    ) {
+      return default_value[i];
+    }
+  }
+};
+const getSumOfBadge = (bags) => {
+  let prioritySum = 0;
+  while (bags.length !== 0) {
+    const res = bags.splice(0, 3);
+    const value = getKeyOfGroup(res);
+    prioritySum = prioritySum + getNumberOfKeyArticle(value);
+  }
+  return prioritySum;
+};
+
+module.exports = { getSumOfBadge,getRucksackNumberOfCompartiments };
--- day3/day3.js
+++ day3/day3.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const getRucksackNumberOfCompartiments = require("./Functions");
+const {getSumOfBadge, getRucksackNumberOfCompartiments} = require("./Functions");
 
 fs.readFile("./data.txt", "utf8", function (err, data) {
   const content = data;
@@ -8,10 +8,14 @@ fs.readFile("./data.txt", "utf8", function (err, data) {
   let bags = content.toString().split("\n");
   let prioritySum = 0
 
+  //par 1
   bags.map((bag)=>{
     prioritySum = prioritySum + getRucksackNumberOfCompartiments(bag);
   })
 
-  console.log("values ", prioritySum);
+  //part 2
+  const prioritySumPart2 = getSumOfBadge(bags)
+  console.log("part 1", prioritySum);
+  console.log("part 2 ", prioritySumPart2);
 
 });
